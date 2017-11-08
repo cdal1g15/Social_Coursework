e99b5e1fe4f6ab949f125628f2b0cfabe0e1788a
Conor Leech <cdal1g15@soton.ac.uk>
2017-11-08 11:28:27 +0000

    Deleted shit

--- Similarity.java
+++ Similarity.java
@@ -1,7 +1,21 @@
+import java.util.HashMap;
+
 /**
  * Check similarity here
  */
 public class Similarity {
+    private HashMap<Integer, HashMap<Integer,Double>> ratingHash;
+
+    public Similarity(HashMap amsterdam){
+        ratingHash=amsterdam;
+    }
+
+    public void topHalfSum(int user1, int user2){
+        HashMap<Integer,Double> user1Hash = ratingHash.get(user1);
+        HashMap<Integer,Double> user2Hash = ratingHash.get(user2);
+
+
+    }
 
 
 }
